a05684489135c63e8cb3bd4f890f420dae329a0b
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-21 16:38:08 +0100

    feat(auth): implement AddUserModal component with modal functionality

--- apps/frontend/src/app/components/shared/Modals/AddUserModal.tsx
+++ apps/frontend/src/app/components/shared/Modals/AddUserModal.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Modal from "./Modal";
+import useAddUserModal from "@/app/hooks/use-user-Modal";
 
 const AddUserModal = () => {
+  const { isOpen, onClose } = useAddUserModal();
   return (
-    <div>AddUserModal</div>
-  )
-}
+    <Modal
+      title="Add User"
+      description="Fill in the details to create a new user account"
+      isOpen={isOpen}
+      onChange={onClose}
+    >
+      <p>Form goes here</p>
+    </Modal>
+  );
+};
 
-export default AddUserModal
+export default AddUserModal;
