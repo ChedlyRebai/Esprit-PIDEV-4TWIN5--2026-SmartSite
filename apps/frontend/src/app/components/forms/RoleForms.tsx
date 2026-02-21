6fe6481c6f954804b5aae0fe6cf62a46cef79595
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-21 17:13:13 +0100

    feat(auth): enhance RoleForms component with validation and improved UI elements

--- apps/frontend/src/app/components/forms/RoleForms.tsx
+++ apps/frontend/src/app/components/forms/RoleForms.tsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import {
+  FieldGroup,
+  Field,
+  FieldLabel,
+  FieldError,
+  FieldDescription,
+} from "@/components/ui/field";
+import {
+  InputGroup,
+  InputGroupTextarea,
+  InputGroupAddon,
+  InputGroupText,
+} from "@/components/ui/input-group";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import * as z from "zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(5, "Role name must be at least 5 characters.")
+    .max(32, "Role name must be at most 32 characters."),
+  description: z
+    .string()
+    .min(20, "Description must be at least 20 characters.")
+    .max(100, "Description must be at most 100 characters."),
+});
+
 
 const RoleForms = () => {
-  return (
-    <div>RoleForms</div>
-  )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
 
-export default RoleForms
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    toast.success("You submitted the following values:");
+  }
+  return (
+    <>
+      <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
+        <FieldGroup>
+          <Controller
+            name="name"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-rhf-demo-name">Role Name</FieldLabel>
+                <Input
+                  {...field}
+                  id="form-rhf-demo-name"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Enter role name"
+                  autoComplete="off"
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+          <Controller
+            name="description"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="form-rhf-demo-description">
+                  Description
+                </FieldLabel>
+                <InputGroup>
+                  <InputGroupTextarea
+                    {...field}
+                    id="form-rhf-demo-description"
+                    placeholder="Enter role description"
+                    rows={6}
+                    className="min-h-24 resize-none"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  <InputGroupAddon align="block-end">
+                    <InputGroupText className="tabular-nums">
+                      {field.value.length}/100 characters
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+        </FieldGroup>
+      </form>
+
+      <Field className="justify-end" orientation="horizontal">
+        <Button type="button" variant="outline" onClick={() => form.reset()}>
+          Reset
+        </Button>
+        <Button type="submit" form="form-rhf-demo">
+          Submit
+        </Button>
+      </Field>
+    </>
+  );
+};
+
+export default RoleForms;
