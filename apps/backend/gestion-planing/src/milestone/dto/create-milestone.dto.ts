15b912d81c1f0fa9442f25bc7482c92991cbb2f2
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-03-18 02:20:22 +0100

    feat: implement CreateMilestoneDto with validation and optional properties

--- apps/backend/gestion-planing/src/milestone/dto/create-milestone.dto.ts
+++ apps/backend/gestion-planing/src/milestone/dto/create-milestone.dto.ts
@@ -1,1 +1,13 @@
-export class CreateMilestoneDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateMilestoneDto {
+    @IsNotEmpty()
+    name: string
+    description?: string
+    projectId?: string
+    siteId?: string;
+    createdBy?: string
+    updatedBy?: string;
+    startDate?: Date
+    endDate?: Date
+}
