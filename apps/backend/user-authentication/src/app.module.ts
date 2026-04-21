89fd3e71f78c12c26e6dff5a801c71c6a3ef0874
asma <asmamhamdi@gmail.com>
2026-04-21 15:56:48 +0100

    sauvegarde avant pull

--- apps/backend/user-authentication/src/app.module.ts
+++ apps/backend/user-authentication/src/app.module.ts
@@ -14,11 +14,7 @@ import { StaticsModule } from './statics/statics.module';
 import { AuditLogsModule } from './audit-logs/audit-logs.module';
 import { TeamsModule } from './teams/teams.module';
 import { ChatbotModule } from './chatbot/chatbot.module';
-<<<<<<< HEAD
-=======
 import { AiChatModule } from './ai-chat/ai-chat.module';
-import { SuppliersModule } from './suppliers/suppliers.module';
->>>>>>> main
 import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
@@ -38,15 +34,10 @@ import { CatalogModule } from './catalog/catalog.module';
     ChatbotModule,
     AiChatModule,
     AuditLogsModule,
-<<<<<<< HEAD
-    CatalogModule,
-=======
 
-    SuppliersModule,
+   // SuppliersModule,
     CatalogModule,
-    SuppliersMaterialsModule,
 
->>>>>>> main
     MongooseModule.forRoot(
       process.env.MONGODB_URI || 'mongodb://localhost:27017/smartsite',
     ),
