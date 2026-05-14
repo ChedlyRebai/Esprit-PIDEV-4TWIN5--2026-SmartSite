888a8047075cb1fd5ac6f41d1f2aac95d7d4d4b8
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-05-14 11:22:18 +0100

    update

--- apps/backend/gestion-planing/src/task/task.module.ts
+++ apps/backend/gestion-planing/src/task/task.module.ts
@@ -2,53 +2,55 @@ import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Milestone, MilestoneSchema } from '../milestone/entities/milestone.entity';
+import {
+  Milestone,
+  MilestoneSchema,
+} from '../milestone/entities/milestone.entity';
 import { Task, TaskSchema } from './entities/task.entity';
-import { TaskStage, TaskStageSchema } from '../task-stage/entities/TaskStage.entities';
+import {
+  TaskStage,
+  TaskStageSchema,
+} from '../task-stage/entities/TaskStage.entities';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
-      MongooseModule.forFeature([
-        { name: Milestone.name, schema: MilestoneSchema },
-        { name: Task.name, schema: TaskSchema },
-        { name: TaskStage.name, schema: TaskStageSchema },
-      ]),
-      
-      ClientsModule.register([
-  {
-    name: 'NOTIFICATION_SERVICE',
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        clientId:
-          process.env.KAFKA_CLIENT_ID ?? 'gestion-planing',
+    MongooseModule.forFeature([
+      { name: Milestone.name, schema: MilestoneSchema },
+      { name: Task.name, schema: TaskSchema },
+      { name: TaskStage.name, schema: TaskStageSchema },
+    ]),
 
-        brokers: [
-          process.env.KAFKA_BROKER ??
-            'kafka-22f5b619-rebaichedly3-d151.c.aivencloud.com:12294',
-        ],
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: process.env.KAFKA_CLIENT_ID ?? 'gestion-planing',
 
-        ssl: true,
+            brokers: [process.env.KAFKA_BROKER ?? ''],
 
-        sasl: {
-          mechanism: 'plain',
-          username: process.env.KAFKA_USERNAME ?? 'avnadmin',
-          password: process.env.KAFKA_PASSWORD ?? 'AVNS_7NgdKSX4QgHZM2h5QXF',
+            ssl: true,
+
+            sasl: {
+              mechanism: 'plain',
+              username: process.env.KAFKA_USERNAME ?? '',
+              password: process.env.KAFKA_PASSWORD ?? '',
+            },
+          },
+
+          producer: {
+            allowAutoTopicCreation: true,
+          },
+
+          consumer: {
+            groupId: 'gestion-planing-consumer',
+          },
         },
       },
-
-      producer: {
-        allowAutoTopicCreation: true,
-      },
-
-      consumer: {
-        groupId: 'gestion-planing-consumer',
-      },
-    },
-  },
-])
-    ],
+    ]),
+  ],
   controllers: [TaskController],
   providers: [TaskService],
   exports: [TaskService],
